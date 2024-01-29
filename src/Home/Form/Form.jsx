426e458c6b929f36c6c26c29e3a1e60111e15fe9
Manu Pranav <62414791+manupranav@users.noreply.github.com>
2024-01-29 22:54:35 +0530

    Update Form.jsx

--- src/Home/Form/Form.jsx
+++ src/Home/Form/Form.jsx
@@ -1,39 +1,38 @@
-/* eslint-disable react/no-unescaped-entities */
 import { useState } from "react";
-
 
 function Form() {
   const [successMessage, setSuccessMessage] = useState("");
   const [failureMessage, setFailureMessage] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
   const [formErrors, setFormErrors] = useState({});
   const [formData, setFormData] = useState({
     name: "",
-    fatherName: "",
-    motherName: "",
     phoneNumber: "",
     email: "",
     dateOfBirth: "",
     address: "",
     pincode: "",
-    tenthSchoolName: "",
-    tenthPercentage: "",
-    twelfthSchoolName: "",
     twelfthPercentage: "",
+    twelfthStream: "",
     twelfthResult: "pass", // set default value for twelfthResult
+    tenthResult: "pass",
     gender: "", // add gender to formData
     isPostgraduate: "no",
     ugCollege: "", // Added ugCollege state
     ugBranch: "",
     ugPercentage: "", // Added ugPercentage state
+    selectedOption: "",
   });
 
   const renderUGFields = () => {
-    if (selectedOption === 'Bachelors Degree' || selectedOption === 'Medical' || selectedOption === 'Engineering') {
+    if (
+      selectedOption === "Bachelors Degree" ||
+      selectedOption === "Medical" ||
+      selectedOption === "Engineering"
+    ) {
       return (
         <div>
-          <div >
-
-
+          <div>
             <div>
               <label
                 htmlFor="12th-percentage"
@@ -57,13 +56,39 @@ function Form() {
                 </p>
               )}
             </div>
-          </div></div>
-
+            <div>
+              <label
+                htmlFor="12th-stream"
+                className="block text-sm font-semibold leading-6  text-black"
+              >
+                12th Stream
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="number"
+                  name="twelfthStream"
+                  id="12th-stream"
+                  value={formData.twelfthStream}
+                  onChange={handleInputChange}
+                  className=" block w-full rounded-md border-0 px-3.5 py-2 text-black-900 ring-1 ring-inset ring-black-300 placeholder:text-black-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                />
+              </div>
+              {formErrors.twelfthStream && (
+                <p className="text-red-500 text-sm">
+                  {formErrors.twelfthStream}
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
       );
-    } else if (selectedOption === 'Masters Degree') {
+    } else if (selectedOption === "Masters Degree") {
       return (
         <div className="gap-2">
-          <label htmlFor="ugCollege" className="mt-5 block text-sm font-semibold leading-6 text-black">
+          <label
+            htmlFor="ugCollege"
+            className="mt-5 block text-sm font-semibold leading-6 text-black"
+          >
             Undergraduate College:
           </label>
           <input
@@ -75,7 +100,10 @@ function Form() {
             className="block w-full rounded-md border-0 px-3.5 py-2 text-black-900 ring-1 ring-inset ring-black-300 placeholder:text-black-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
           />
 
-          <label htmlFor="ugBranch" className="mt-5 block text-sm font-semibold leading-6 text-black">
+          <label
+            htmlFor="ugBranch"
+            className="mt-5 block text-sm font-semibold leading-6 text-black"
+          >
             Undergraduate Stream:
           </label>
           <input
@@ -87,7 +115,10 @@ function Form() {
             className="block w-full rounded-md border-0 px-3.5 py-2 text-black-900 ring-1 ring-inset ring-black-300 placeholder:text-black-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
           />
 
-          <label htmlFor="ugPercentage" className="mt-5 block text-sm font-semibold leading-6 text-black">
+          <label
+            htmlFor="ugPercentage"
+            className="mt-5 block text-sm font-semibold leading-6 text-black"
+          >
             Undergraduate Percentage:
           </label>
           <input
@@ -100,62 +131,32 @@ function Form() {
           />
         </div>
       );
-    } else {
+    } else if (selectedOption === "Diploma") {
       return (
-
         <div>
-
-          <div >
-            <label
-              htmlFor="10th-result"
-              className="block text-sm font-semibold leading-6  text-black"
+          <label
+            htmlFor="10th-result"
+            className="block text-sm font-semibold leading-6  text-black"
+          >
+            10th Result (Pass/Fail)
+          </label>
+          <div className="mt-2.5">
+            <select
+              id="10th-result"
+              name="tenthResult"
+              value={formData.tenthResult}
+              onChange={handleInputChange}
+              className=" block w-full rounded-md border-0 bg-transparent bg-none px-3.5 py-2 text-black-900 ring-1 ring-inset ring-black-300 placeholder:text-black-400 focus:ring-2 focus:ring-inset sm:text-sm"
             >
-              10th Result (Pass/Fail)
-            </label>
-            <div className="mt-2.5">
-              <select
-                id="10th-result"
-                name="tenthResult"
-                value={formData.tenthResult}
-                onChange={handleInputChange}
-                className=" block w-full rounded-md border-0 bg-transparent bg-none px-3.5 py-2 text-black-900 ring-1 ring-inset ring-black-300 placeholder:text-black-400 focus:ring-2 focus:ring-inset sm:text-sm"
-              >
-                <option value="pass">Pass</option>
-                <option value="fail">Fail</option>
-              </select>
-              {formErrors.tenthResult && (
-                <p className="text-red-500 text-sm">
-                  {formErrors.tenthResult}
-                </p>
-              )}
-            </div>
-            <label
-              htmlFor="10th-percentage"
-              className="block text-sm font-semibold leading-6  text-black"
-            >
-              10th Percentage
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="number"
-                name="tenthPercentage"
-                id="10th-percentage"
-                value={formData.tenthPercentage}
-                onChange={handleInputChange}
-                className=" block w-full rounded-md border-0 px-3.5 py-2 text-black-900 ring-1 ring-inset ring-black-300 placeholder:text-black-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-              />
-            </div>
-            {
-              formErrors.tenthPercentage && (
-                <p className="text-red-500 text-sm">
-                  {formErrors.tenthPercentage}
-                </p>
-              )
-            }
+              <option value="pass">Pass</option>
+              <option value="fail">Fail</option>
+            </select>
+            {formErrors.tenthResult && (
+              <p className="text-red-500 text-sm">{formErrors.tenthResult}</p>
+            )}
           </div>
 
-          <div >
-
+          <div>
             <div>
               <label
                 htmlFor="12th-percentage"
@@ -182,17 +183,20 @@ function Form() {
           </div>
         </div>
       );
+    } else {
+      return null;
     }
   };
-
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
 
     // Enforce numeric input for specific fields
-    if (["phoneNumber", "tenthPercentage", "twelfthPercentage"].includes(name)) {
+    if (
+      ["phoneNumber", "tenthPercentage", "twelfthPercentage"].includes(name)
+    ) {
       // Allow only numeric values
-      const numericValue = value.replace(/\D/g, ''); // Remove non-numeric characters
+      const numericValue = value.replace(/\D/g, ""); // Remove non-numeric characters
       setFormData((prevData) => ({
         ...prevData,
         [name]: numericValue,
@@ -215,7 +219,6 @@ function Form() {
     }
   };
 
-
   const validateForm = () => {
     const newErrors = {};
     Object.keys(formData).forEach((field) => {
@@ -237,11 +240,9 @@ function Form() {
       errors.name = "Name should only contain characters and space!";
     }
 
-
     if (!formData.dateOfBirth) {
       errors.dateOfBirth = "Date of Birth is required!";
     }
-
 
     if (!formData.twelfthSchoolName) {
       errors.twelfthSchoolName = "School Name is required!";
@@ -256,8 +257,6 @@ function Form() {
     if (formData.email && !regexEmail.test(formData.email)) {
       errors.email = "Invalid email address!";
     }
-
-
 
     // Validation for percentage (assuming it should be a number between 0 and 100)
     const regexPercentage = /^\d{2}$/;
@@ -295,13 +294,16 @@ function Form() {
     }
 
     try {
-      const response = await fetch("https://myfutureway-backend.onrender.com/submitForm", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://myfutureway-backend.onrender.com/submitForm",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       if (response.ok) {
         // Handle success, e.g., show a success message
@@ -311,17 +313,15 @@ function Form() {
         // Clear form data
         setFormData({
           name: "",
-
           phoneNumber: "",
           email: "",
           dateOfBirth: "",
           address: "",
-
-          tenthPercentage: "",
-          twelfthSchoolName: "",
           twelfthPercentage: "",
           twelfthResult: "pass",
           gender: "",
+          selectedOption: "",
+          tenthResult: "pass",
         });
       } else {
         // Handle errors, e.g., show an error message
@@ -333,12 +333,8 @@ function Form() {
       console.error("Error submitting form", error);
       setSuccessMessage(""); // Clear success message if any
       setFailureMessage("Error sending");
-
     }
   };
-
-
-  const [selectedOption, setSelectedOption] = useState('');
 
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value);
@@ -346,7 +342,9 @@ function Form() {
 
   return (
     <div className="isolate px-6 py-24 sm:py-32 lg:px-8">
-      <p className=" text-center text-[#20B486] font-bold">Your future journey begins with a simple message.</p>
+      <p className=" text-center text-[#20B486] font-bold">
+        Your future journey begins with a simple message.
+      </p>
       <form
         onSubmit={handleSubmit}
         action="#"
@@ -359,7 +357,7 @@ function Form() {
               htmlFor="name"
               className="block text-sm font-semibold leading-6  text-black"
             >
-              Name
+              Fullname
             </label>
             <div className="mt-2.5">
               <input
@@ -375,9 +373,6 @@ function Form() {
               <p className="text-red-500 text-sm">{formErrors.name}</p>
             )}
           </div>
-
-
-
 
           <div>
             <label
@@ -520,7 +515,12 @@ function Form() {
           )}
         </div>
         <div>
-          <label htmlFor="isPostgraduate" className="mt-5 block text-sm font-semibold leading-6  text-black">Looking For</label>
+          <label
+            htmlFor="isPostgraduate"
+            className="mt-5 block text-sm font-semibold leading-6  text-black"
+          >
+            Looking For
+          </label>
           <ul className="items-center w-full text-sm font-medium text-black-900 bg-white border border-black-200 rounded-lg sm:flex">
             <li className="w-full border-b border-black-200 sm:border-b-0 sm:border-r dark:border-black-600">
               <div className="flex items-center ps-3">
@@ -537,7 +537,6 @@ function Form() {
                   className="w-full py-3 ms-2 text-sm font-medium text-black-900 dark:text-black-300"
                 >
                   Diploma
-                  {/* need 10th p / f  if pass +2 Stream and percenatge else 10th per  */}
                 </label>
               </div>
             </li>
@@ -547,7 +546,7 @@ function Form() {
                 <input
                   id="horizontal-list-radio-license"
                   type="radio"
-                  value=" Bachelors Degree"
+                  value="Bachelors Degree"
                   name="list-radio"
                   onChange={handleOptionChange}
                   className="w-4 h-4 text-blue-600 bg-black-100 border-black-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-black-700 dark:focus:ring-offset-black-700 focus:ring-2 dark:bg-black-600 dark:border-black-500"
@@ -557,7 +556,6 @@ function Form() {
                   className="w-full py-3 ms-2 text-sm font-medium text-black-900 dark:text-black-300"
                 >
                   Bachelors Degree
-                  {/* 10 and 12 stream and , percentage for both 10 and 12 */}
                 </label>
               </div>
             </li>
@@ -577,7 +575,6 @@ function Form() {
                 >
                   Masters Degree
                   {/*  ug percentage and stream */}
-
                 </label>
               </div>
             </li>
@@ -597,7 +594,6 @@ function Form() {
                 >
                   Medical
                   {/* 10 and 12 stream and , percentage for both 10 and 12 */}
-
                 </label>
               </div>
             </li>
@@ -606,7 +602,7 @@ function Form() {
                 <input
                   id="horizontal-list-radio-license"
                   type="radio"
-                  value=" Engineering"
+                  value="Engineering"
                   name="list-radio"
                   onChange={handleOptionChange}
                   className="w-4 h-4 text-blue-600 bg-black-100 border-black-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-black-700 dark:focus:ring-offset-black-700 focus:ring-2 dark:bg-black-600 dark:border-black-500"
@@ -617,7 +613,6 @@ function Form() {
                 >
                   Engineering
                   {/* 10 and 12 stream and , percentage for both 10 and 12 */}
-
                 </label>
               </div>
             </li>
@@ -643,7 +638,6 @@ function Form() {
           <button
             type="submit"
             className="block w-full rounded-md bg-[#20B486] border px-3.5 py-2.5 text-center text-sm font-semibold text-white hover:shadow-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
-
           >
             Send
           </button>
@@ -659,7 +653,6 @@ function Form() {
             </div>
           )}
         </div>
-
       </form>
     </div>
   );
