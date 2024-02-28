519741a12f99ee7e296ba6f7cff9335c0dad79cf
Sonu S <sonusuresh908@gmail.com>
2024-02-28 14:57:08 +0530

    404

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Landing from './Home/Landing'
-
+// import Landing from './Home/Landing'
+import NotFound from './Home/NotFound'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<Landing />} />     
+      {/* <Route path='/' element={<Landing />} />      */}
+      <Route path='/' element={<NotFound />} />     
        </Routes>
 
     </>
